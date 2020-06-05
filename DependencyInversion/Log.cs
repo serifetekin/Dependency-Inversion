324de4174309d4f046b4850759904ea911554634
Şerife Tekin <serifetekin2451@gmail.com>
2020-06-05 16:09:18 +0300

    Solid prensiplerinden Dependency Inversion

---
+++ DependencyInversion/Log.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversion
+{
+
+    interface ILogger
+    {
+        void Yaz(string yazi);
+    }
+    class SQLeLogla : ILogger
+    {
+        public void Yaz(string yazi)
+        {
+            Console.WriteLine("Sqle Logla " + yazi);
+        }
+    }
+    class FileLogla : ILogger
+    {
+        public void Yaz(string yazi)
+        {
+            Console.WriteLine("File Logla " + yazi);
+        }
+    }
+    class KullaniciDb
+    {
+        ILogger log;
+
+
+        public KullaniciDb(ILogger _log)
+        {
+            log = _log;
+        }
+        public void Kaydet()
+        {
+            log.Yaz("Kaydet");
+        }
+
+        public void Sil()
+        {
+            log.Yaz("Sil");
+        }
+    }
+}
---
+++ DependencyInversion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversion
+{
+    class Program
+    {
+        // bir class başka bir class kullanıyor. kullanılan classa alt sınıf kullanan sınıfa üst sınıf deniyor.
+        static void Main(string[] args)
+        {
+            KullaniciDb kd = new KullaniciDb(new SQLeLogla());
+
+            kd.Kaydet();
+
+            KullaniciDb kd2 = new KullaniciDb(new FileLogla());
+
+            kd2.Kaydet();
+
+            Console.ReadLine();
+
+        }
+    }
+}
